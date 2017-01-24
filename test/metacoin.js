249060972da9a7ab64b84ff78a2e6e531e4db8a2
Will <Will@Wills-MacBook-Air.local>
2017-01-24 16:38:08 +1100

    fix metacoin test

--- test/metacoin.js
+++ test/metacoin.js
@@ -1,9 +1,9 @@
 contract('MetaCoin', function(accounts) {
-  it("should put 10000 MetaCoin in the first account", function() {
+  it("should put 50000 MetaCoin in the first account", function() {
     var meta = MetaCoin.deployed();
 
     return meta.getBalance.call(accounts[0]).then(function(balance) {
-      assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+      assert.equal(balance.valueOf(), 50000, "50000 wasn't in the first account");
     });
   });
   it("should call a function that depends on a linked library", function() {
