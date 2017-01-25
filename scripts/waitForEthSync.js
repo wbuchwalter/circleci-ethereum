1fb1c49963fd482dff268c80b3311c579f075f00
William Buchwalter <wbuchwalter@gmail.com>
2017-01-25 14:36:33 +1100

    update waitforsync script

--- scripts/waitForEthSync.js
+++ scripts/waitForEthSync.js
@@ -1,42 +1,40 @@
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+attemptConnection('http://0.0.0.0:8545').then(waitForSync).catch(console.log)
 
-const waitForSync = _ =>  { 
-    if(web3.eth.syncing) {
-        console.log('Sync in progress')
-        waitForSync()
-    } else {
-        console.log('Sync completed')
-    }
+function attemptConnection(uri){
+    return new Promise((resolve, reject) => {
+        const maxConnectionTries = 10
+        let attemptNumber = 0
+        const connect = () => {
+            try {
+                attemptNumber += 1
+                let web3 = new Web3(new Web3.providers.HttpProvider(uri))
+                console.log('Connection to provider successful')
+                resolve(web3)
+            } catch(err) {
+                console.log(err)
+                if(attemptNumber < maxConnectionTries) {
+                    console.log('Connection to provider failed, retrying...')
+                    setTimeout(() => connect(), 2000)
+                } else {
+                    reject('Could not connect to provider.')
+                }
+            }
+        }
+        connect()
+    })
 }
 
-waitForSync()
-
-
-web3.eth.isSyncing(function(error, sync){
-    if(!error) {
-        // stop all app activity
-        if(sync === true) {
-           console.log('sync == true')
-
-        // show sync info
-        } else if(sync) {
-           console.log('sync == false') 
-           console.log(sync.currentBlock);
-
-        // re-gain app operation
-        } else {
-            // run your app init function...
-           console.log('sync is null')
+var firstPass = true
+function waitForSync(web3) {
+    if (web3.isConnected() && !web3.eth.syncing && web3.eth.blockNumber > 0) {
+        console.log('Done')
+    } else {
+        if (firstPass) {
+            console.log('Waiting for sync to finish...')
+            firstPass = false
         }
+        setTimeout(() => waitForSync(web3), 2000)
     }
-});
-
-console.log(`Status: `)
-console.log(`Connected: ${web3.isConnected()}`)
-console.log(`Syncing: ${web3.syncing}`)
-console.log(`Block#: ${web3.eth.blockNumber}`)
-
-
-
+}
 
