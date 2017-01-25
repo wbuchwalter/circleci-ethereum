ef4cc49420067d328620de25a9c4888c01e779ec
William Buchwalter <wbuchwalter@gmail.com>
2017-01-25 11:48:35 +1100

    wait for eth sync script

---
+++ scripts/waitForEthSync.js
@@ -0,0 +1,7 @@
+const Web3 = require('web3')
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+console.log(`web3 status: ${web3}`)
+const waitForSync = _ =>  { if(web3.eth.syncing) { waitForSync() } }
+waitForSync()
+console.log('Sync completed')
